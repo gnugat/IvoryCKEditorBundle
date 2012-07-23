bc86cef4bedfb975842764468462cb102f05aa7f
Eric GELOEN <geloen.eric@gmail.com>
2012-07-23 10:41:31 +0200

    [Form] Fix builView according to symfony master branch

--- Form/Type/CKEditorType.php
+++ Form/Type/CKEditorType.php
@@ -4,7 +4,7 @@ namespace Ivory\CKEditorBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormViewInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
@@ -29,12 +29,13 @@ class CKEditorType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function buildView(FormViewInterface $view, FormInterface $form, array $options)
+    public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        $view
-            ->setVar('toolbar', $form->getAttribute('toolbar'))
-            ->setVar('ui_color', $form->getAttribute('ui_color'))
-            ->setVar('allow_upload', $form->getAttribute('allow_upload'));
+        $view->vars = array_replace($view->vars, array(
+            'toolbar'      => $form->getAttribute('toolbar'),
+            'ui_color'     => $form->getAttribute('ui_color'),
+            'allow_upload' => $form->getAttribute('allow_upload'),
+        ));
     }
 
     /**
